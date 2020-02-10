62c215749ee1369e2140caf8def6f6f8ab14f3ad
Anis-Barghouthi <anis.barghouthi@outlook.com>
2020-02-10 11:59:36 +0100

    dup2

--- 10-02/dup2.c
+++ 10-02/dup2.c
@@ -1,3 +1,25 @@
-int main(){
-    
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+    int p[2];
+    if (pipe(p))
+    {
+        perror("Erreur de création de tube");
+        return 0;
+    }
+    if (fork())
+    {
+        close(p[0]);
+        dup2(p[1], 1); // stdout = 1
+        close(p[1]);
+        execlp("ls", "ls", "-l", NULL);
+    }
+    else
+    {
+        close(p[1]);
+        dup2(p[0], 0); // stdin =0
+        close(p[0]);
+        execlp("wc", "wc", NULL);
+    }
 }
