6c6a01a5b708aea93e4d68e3c2c028d19c5c5748
Anis-Barghouthi <anis.barghouthi@outlook.com>
2020-02-17 10:33:44 +0100

    pthread_exit(NULL) -> pthread_join(NULL)

--- 17-02/thread1.c
+++ 17-02/thread1.c
@@ -14,13 +14,13 @@ int main()
 {
     pthread_t th1;
     int arg = 5;
-    int r = 0;
-    r = pthread_create(&th1, NULL, afficher, &arg);
-    if (r == -1)
+
+    if (pthread_create(&th1, NULL, afficher, &arg))
     {
         perror("error de creation de thread");
         pthread_exit(NULL);
     }
+
     pthread_join(th1, NULL);
     return 1;
 }
