986aab437afc6e00a9d1e0e75b65806fd1d8acf4
hirodemon <jedfakhfekh@gmail.com>
2020-03-09 02:21:41 +0100

    Update code3-2.c

--- 3-02/1/code3-2.c
+++ 3-02/1/code3-2.c
@@ -2,7 +2,7 @@ int main(int argc, char *argv[])
 // ./prog     3
 // argv[0] argv[1]
 {
-    int cp = 0, i = atoi(argv[1]);
+    int cp = 0, i = atoi(argv[1]); //Parses the C-string str interpreting its content as an integral number, which is returned as a value of type int.
     while (cp < i)
     {
         if (fork() > 0)
